503e09bc913260f1b54acc9c88c6e3e285f323e0
Petr Pavlik <petrpavlik@me.com>
2025-08-12 23:58:37 +0200

    Remove commented-out Swift concurrency settings from Package.swift

--- Package.swift
+++ Package.swift
@@ -21,17 +21,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Tolgee",
-            // swiftSettings: [
-            //     .enableUpcomingFeature("StrictConcurrency")
-            // ]
+            name: "Tolgee"
         ),
         .testTarget(
             name: "TolgeeTests",
-            dependencies: ["Tolgee"],
-            // swiftSettings: [
-            //     .enableUpcomingFeature("StrictConcurrency")
-            // ]
+            dependencies: ["Tolgee"]
         ),
     ]
 )
